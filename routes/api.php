92e6d88a5d58f9e52f158ba60f69fc00cdddea4b
ElieOko <elieoko100@gmail.com>
2025-01-20 07:25:50 +0100

    fix:conflic

--- routes/api.php
+++ routes/api.php
@@ -10,11 +10,10 @@ use App\Http\Controllers\ProfessionController;
 Route::post('user/register', [UserController::class, 'register']);
 Route::post('user/login', [UserController::class, 'login']);
 Route::resource('profession', ProfessionController::class)->only(['index']);
+Route::post('personnel/filter', [PersonnelController::class, 'filter']);
+
 Route::middleware(['auth:sanctum'])->group(function () { 
-    
     Route::resource('personnel', PersonnelController::class)->only(['index', 'store']);
-
-
 })->missing(function (Request $request) {
     return response()->json([
         "message" => "url not found ".$request->fullUrl()
